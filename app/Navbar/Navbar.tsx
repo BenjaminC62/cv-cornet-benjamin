11f759f3d4c209044d05dbb3b6292096f2214b58
BenjaminC62 <benjamincornet62130@gmail.com>
2025-12-09 15:59:00 +0100

    #2 - ajout de la box pour la recherche de stage

--- app/Navbar/Navbar.tsx
+++ app/Navbar/Navbar.tsx
@@ -3,10 +3,21 @@ import Avatar from './components/Avatar';
 
 const Navbar: React.FC = () => {
     return (
-        <nav className='flex'>
-            <Avatar />
-            <span>Cornet Benjamin</span>
-            <span>Etudiant à l'iut de lens</span>
+        <nav className=''>
+            <div className='flex items-center gap-4'>
+                <Avatar />
+                <div className='flex flex-col text-white'>
+                    <span>Cornet Benjamin</span>
+                    <span>Etudiant à l'iut de lens</span>
+                </div>
+            </div>
+            <div className="mt-8 bg-white/10 backdrop-blur-md p-4 rounded-xl border border-white/20 flex items-center gap-4 shadow-sm">
+                <span className="text-2xl">🎯</span>
+                
+                <span className="text-white font-medium text-lg">
+                    Recherche stage développement web à partir du 16 mars
+                </span>
+            </div>
         </nav>
     )
 }
