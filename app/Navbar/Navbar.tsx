75a7428b7c1509287fe10477016a7803ef438c35
BenjaminC62 <benjamincornet62130@gmail.com>
2025-12-09 16:27:08 +0100

    #9 - Ajout des font optimiser dans la navbar

--- app/Navbar/Navbar.tsx
+++ app/Navbar/Navbar.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Avatar from './components/Avatar';
+import { poppins } from '@/components/ui/font';
+import ContactBar from './components/ContactBar';
 
 const Navbar: React.FC = () => {
     return (
-        <nav className=''>
+        <nav className='flex flex-col'>
             <div className='flex items-center gap-4'>
                 <Avatar />
                 <div className='flex flex-col text-white'>
-                    <span>Cornet Benjamin</span>
-                    <span>Etudiant à l'iut de lens</span>
+                    <span className={`${poppins.className} antialiased text-[52px] uppercase font-bold leading-none`}>Cornet Benjamin</span>
+                    <span className={`${poppins.className} antialiased text-[16px] mt-1`}>Etudiant à l'iut de lens</span>
                 </div>
             </div>
             <div className="mt-8 bg-white/10 backdrop-blur-md p-4 rounded-xl border border-white/20 flex items-center gap-4 shadow-sm">
@@ -18,6 +20,7 @@ const Navbar: React.FC = () => {
                     Recherche stage développement web à partir du 16 mars
                 </span>
             </div>
+            <ContactBar />
         </nav>
     )
 }
