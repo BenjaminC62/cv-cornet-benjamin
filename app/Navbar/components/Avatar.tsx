da5e29c8d9e30b78419db84432ae511b3aba49fc
BenjaminC62 <benjamincornet62130@gmail.com>
2025-12-09 15:32:26 +0100

    Fixes #6 - Création du component de l'avatar

---
+++ app/Navbar/components/Avatar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Avatar: React.FC = () => {
+  return (
+    <div className="">
+      <div className='bg-white rounded-full w-24 h-24 shadow-md shadow-white-500/50'></div>
+    </div>
+  )
+}
+
+export default Avatar;
