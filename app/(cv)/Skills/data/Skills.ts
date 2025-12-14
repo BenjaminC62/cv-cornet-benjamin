d181b4380b8313a506cf30b46135979af8a4894b
BenjaminC62 <benjamincornet62130@gmail.com>
2025-12-14 17:02:03 +0100

    Fixes #11 - ajout des différents compétances dans un fichier ts pour les utilisers en tant que props

---
+++ app/(cv)/Skills/data/Skills.ts
@@ -0,0 +1,8 @@
+export const skills = [
+    { name: "React", level: "Avancé", icon: "⚛️", color: "text-blue-500", colorDot: "bg-blue-500" },
+    { name: "Next.js", level: "Intermédiaire", icon: "▲", color: "text-black", colorDot: "bg-black" },
+    { name: "Tailwind CSS", level: "Avancé", icon: "🎨", color: "text-cyan-500", colorDot: "bg-cyan-500" },
+    { name: "TypeScript", level: "En apprentissage", icon: "📘", color: "text-blue-600", colorDot: "bg-blue-600" },
+    { name: "Git & GitHub", level: "Indispensable", icon: "🐙", color: "text-gray-700", colorDot: "bg-gray-700" },
+    { name: "Figma", level: "Design", icon: "🖌️", color: "text-purple-500", colorDot: "bg-purple-500" },
+];
