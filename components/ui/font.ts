4e2fa76cc200bf47800daddb2efbb914d6616be5
BenjaminC62 <benjamincornet62130@gmail.com>
2025-12-20 12:37:48 +0100

    #9 - Delete les fonts pour les imports via css

--- components/ui/font.ts
+++
@@ -1,4 +0,0 @@
-import { Inter, Poppins } from 'next/font/google';
-
-export const inter = Inter({ subsets: ['latin'], variable: '--font-inter', display: 'swap'});
-export const poppins = Poppins({ weight: ['400', '600', '700'], subsets: ['latin'], variable: '--font-poppins', display: 'swap'});
